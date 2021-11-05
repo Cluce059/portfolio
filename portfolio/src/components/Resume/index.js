9d0e163f8c369ae1a1be7bf706a402140965c86a
caroline <cluce059@gmail.com>
2021-11-04 21:49:38 -0400

    correctionssss

--- portfolio/src/components/Resume/index.js
+++ portfolio/src/components/Resume/index.js
@@ -7,9 +7,10 @@ function Resume(){
         <h1 className="flex-row">Resume</h1>
         <h2 className="flex-row">Proficiencies</h2>
         <ul>
-            <li>Javascript and also JSX with React</li>
+            <li>JavaScript</li>
+            <li>MERN stack development</li>
             <li>Back-End Web Development</li>
-            <li>Database Management</li>
+            <li>NoSQl and SQL Database Management</li>
             <a href={resume}>Download Resume PDF</a>
         </ul>
         </section>
